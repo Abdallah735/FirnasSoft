ae048315c061b188cb2acff9bffefc09b1f62fd6
samir <abdallahsamir735@gmail.com>
2025-11-01 11:44:56 +0200

    final which bit slow

--- UDP/client/client.go
+++ UDP/client/client.go
@@ -430,11 +430,11 @@ func (c *Client) requestManagerForFile(uuid string, totalChunks int, chunkSize i
 	replyCount := make(chan any)
 	c.receivingStateChan <- ReceivingCommand{Action: "getCount", Key: uuid, Reply: replyCount}
 	totalRec := (<-replyCount).(int)
-	if totalRec+1 >= totalChunks {
+	if totalRec >= totalChunks {
 		c.packetGenerator(_transfer_complete, []byte(uuid), 0, nil, nil)
-	} else {
+	} /*else {
 		fmt.Printf("File %s partially received (%d/%d). You may retry later.\n", uuid, totalRec, totalChunks)
-	}
+	}*/
 }
 func (c *Client) SendFileToServer(path string) error {
 	f, err := os.Open(path)
